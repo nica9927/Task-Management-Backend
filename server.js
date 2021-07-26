b46930562e49da0b2016ec1bb5d53022ecf67866
caca9927 <600112416012@bru.ac.th>
2021-07-26 14:50:13 +0700

    edit update

--- server.js
+++ server.js
@@ -33,50 +33,50 @@ let db = mysql.createConnection({
 ///////////// TASKS ////////////////////////////
 
 //Get All Tasks
-app.get('/getAllTasks',(req,res) => {
-    db.query("call sp_get_tasks()", (err,result)=>{
-        if(err) throw err;
-        let message = ""
-        if(result === undefined || result.length == 0){
-            message = "you don't have any task!";
-            return res.json({message: message});
-        }
-        return res.json(result);
-    })
-})
+// app.get('/getAllTasks',(req,res) => {
+//     db.query("call sp_get_tasks()", (err,result)=>{
+//         if(err) throw err;
+//         let message = ""
+//         if(result === undefined || result.length == 0){
+//             message = "you don't have any task!";
+//             return res.json({message: message});
+//         }
+//         return res.json(result);
+//     })
+// })
 
 // Get Task By ID
-app.get('/getTask', (req,res) => {
-    let id = req.query.id;
-    if(!id){
-        return res.status(400).json({message: "Please Provide Task ID!"});
-    }else{
-        db.query("select * from tasks,projects,members,statuses where member_id = members.id and project_id = projects.id and tasks.status_id = statuses.id and tasks.id = ?", id, (err,result)=>{
-            if(err) throw err;
-            let message = ""
-            if(result === undefined || result.length == 0){
-                message = "Task not found.";
-                return res.json({message:message});
-            }
-            return res.json(result);
-        })
-    }
-})
+// app.get('/getTask', (req,res) => {
+//     let id = req.query.id;
+//     if(!id){
+//         return res.status(400).json({message: "Please Provide Task ID!"});
+//     }else{
+//         db.query("select * from tasks,projects,members,statuses where member_id = members.id and project_id = projects.id and tasks.status_id = statuses.id and tasks.id = ?", id, (err,result)=>{
+//             if(err) throw err;
+//             let message = ""
+//             if(result === undefined || result.length == 0){
+//                 message = "Task not found.";
+//                 return res.json({message:message});
+//             }
+//             return res.json(result);
+//         })
+//     }
+// })
 
-// Add New Task
-app.post('/addTask',(req,res)=>{
-    let title = req.body.title;
-    let detail = req.body.detail;
+// // Add New Task
+// app.post('/addTask',(req,res)=>{
+//     let title = req.body.title;
+//     let detail = req.body.detail;
 
-    if(!title || !detail){
-        return res.status(400).send({message: "Please provide title and detail!"});
-    }else{
-        db.query('INSERT INTO tasks (task_title, task_detail) VALUES(?,?)',[title,detail], (err,result)=>{
-            if(err) throw err;
-            return res.json({data:{title:title,detail:detail},message:"Task added successfully"});
-        })
-    }
-})
+//     if(!title || !detail){
+//         return res.status(400).send({message: "Please provide title and detail!"});
+//     }else{
+//         db.query('INSERT INTO tasks (task_title, task_detail) VALUES(?,?)',[title,detail], (err,result)=>{
+//             if(err) throw err;
+//             return res.json({data:{title:title,detail:detail},message:"Task added successfully"});
+//         })
+//     }
+// })
 
 
 /////////////////////////////////////////////////////////////////////
@@ -99,24 +99,24 @@ app.get('/getAllMembers', (req, res) => {
 })
 
 // Get Member By ID
-app.get('/getMember',(req,res)=> {
-    let id = req.query.id;
+// app.get('/getMember',(req,res)=> {
+//     let id = req.query.id;
 
-    if(!id){
-        return res.status(400).send({message: "Please Provide Member ID!"});
-    }else{
-        db.query("SELECT * FROM members WHERE id = ?", id, (err,result)=>{
-            if(err)throw err;
-            let message = ""
-            if(result === undefined || result.length == 0){
-                message = "Member not found";
-                return res.send({message:message});
-            }else{
-                return res.send(result);
-            }
-        })
-    }
-})
+//     if(!id){
+//         return res.status(400).send({message: "Please Provide Member ID!"});
+//     }else{
+//         db.query("SELECT * FROM members WHERE id = ?", id, (err,result)=>{
+//             if(err)throw err;
+//             let message = ""
+//             if(result === undefined || result.length == 0){
+//                 message = "Member not found";
+//                 return res.send({message:message});
+//             }else{
+//                 return res.send(result);
+//             }
+//         })
+//     }
+// })
 
 
 
@@ -127,36 +127,36 @@ app.get('/getMember',(req,res)=> {
 ///////////// PROJECTS ////////////////////////////
 
 // Get All Projects
-app.get('/getAllProjects', (req,res)=> {
-    db.query("SELECT * FROM projects", (err,result)=>{
-        if(err) throw err;
-        let message = ""
-        if(result === undefined || result.length == 0){
-            message = "You don't have any projects right now!";
-            return res.json({message:message});
-        }
-        return res.json(result);
-    })
-})
+// app.get('/getAllProjects', (req,res)=> {
+//     db.query("SELECT * FROM projects", (err,result)=>{
+//         if(err) throw err;
+//         let message = ""
+//         if(result === undefined || result.length == 0){
+//             message = "You don't have any projects right now!";
+//             return res.json({message:message});
+//         }
+//         return res.json(result);
+//     })
+// })
 
-// Get Project by ID
-app.get('/getProject',(req,res)=>{
-    let id = req.query.id;
+// // Get Project by ID
+// app.get('/getProject',(req,res)=>{
+//     let id = req.query.id;
 
-    if(!id){
-        return res.status(400).send({message: "Please provide project id"})
-    }else{
-        db.query("SELECT * FROM projects WHERE id = ?",id,(err,result)=>{
-            if(err) throw err;
-            let message = ""
-            if(result === undefined || result.length == 0){
-                message = "Project not found";
-                return res.send({message : message})
-            }
-            return res.send(result);
-        })
-    }
-})
+//     if(!id){
+//         return res.status(400).send({message: "Please provide project id"})
+//     }else{
+//         db.query("SELECT * FROM projects WHERE id = ?",id,(err,result)=>{
+//             if(err) throw err;
+//             let message = ""
+//             if(result === undefined || result.length == 0){
+//                 message = "Project not found";
+//                 return res.send({message : message})
+//             }
+//             return res.send(result);
+//         })
+//     }
+// })
 ///////////////////////////////////////////////////////
 
 
